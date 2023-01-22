e24941e4c9364fb4ecd5a0c08dad23c9c28c3c84
Emily <emilyrosesteyn@gmail.com>
2023-01-22 20:22:50 +0200

    Add slurm script

---
+++ slurm/start-jupyter-notebook.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=4G
+#SBATCH --time=05:00:00
+#SBATCH --job-name=jupyter-notebook
+
+# get tunneling info
+node=$(hostname -s)
+user=$(whoami)
+port=8889
+
+# print tunneling instructions jupyter-log
+echo -e "
+Command to create ssh tunnel:
+ssh -N -f -L ${port}:${node}:${port} ${user}@jaguar1.eie.wits.ac.za
+
+Use a Browser on your local machine to go to:
+localhost:${port}  (prefix w/ https:// if using password)
+"
+
+# load modules or conda environments here
+mamba activate africa_poverty_clean
+
+# Run Jupyter
+jupyter-notebook --notebook-dir=.. --no-browser --port=${port} --ip=${node}
